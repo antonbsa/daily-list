cdb56cf76c48623c6907dad64dc7bb9806ddac62
Anton B <antonbsa@gmail.com>
2022-02-16 02:02:42 -0300

    set separated env value to URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 
 const port = process.env.PORT || 8080;
-const url = process.env.VERCEL_URL || `http://localhost:${port}`;
+const url = process.env.URL_BASE || `http://localhost:${port}`;
 
 const api = axios.create({
   baseURL: url,
